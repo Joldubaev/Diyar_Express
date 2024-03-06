3378d89fd465277a937930eb694f9880a944e971
Aibek Karataev <aibekdv@gmail.com>
2024-03-06 21:05:41 +0600

    Add custom input and submit button widgets

---
+++ app/lib/components/submit_button_widget.dart
@@ -0,0 +1,60 @@
+import 'package:diyar_express/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class SubmitButtonWidget extends StatelessWidget {
+  final VoidCallback? onTap;
+  final String title;
+  final Color bgColor;
+  final TextStyle textStyle;
+  final bool? isLoading;
+  final double? height;
+  final double? width;
+  final Color? borderColor;
+
+  const SubmitButtonWidget({
+    super.key,
+    this.onTap,
+    required this.title,
+    required this.bgColor,
+    required this.textStyle,
+    this.isLoading = false,
+    this.height,
+    this.width,
+    this.borderColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: borderColor != null
+            ? borderColor!.withAlpha(1)
+            : AppColors.primaryAccent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(height ?? 55),
+          side: borderColor != null
+              ? BorderSide(color: borderColor!)
+              : BorderSide.none,
+        ),
+        shadowColor: borderColor != null ? Colors.transparent : AppColors.grey1,
+        minimumSize: width != null
+            ? Size(width!, height ?? 50)
+            : Size.fromHeight(height ?? 50),
+        backgroundColor: bgColor,
+      ),
+      child: isLoading == true
+          ? const SizedBox(
+              width: 25,
+              height: 25,
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            )
+          : Text(
+              title,
+              style: textStyle,
+            ),
+    );
+  }
+}
