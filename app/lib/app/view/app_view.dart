0993d31bc3ededb6e296ba0edcbcfb444d98a04c
admin <admin@admins-MacBook-Pro.local>
2024-03-12 11:57:35 +0600

    update pod

--- app/lib/app/view/app_view.dart
+++ app/lib/app/view/app_view.dart
@@ -2,17 +2,14 @@ import 'package:diyar_express/l10n/l10n.dart';
 import 'package:diyar_express/modules/modules.dart';
 import 'package:diyar_express/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: const [],
-      child: const DiyarExpress(),
-    );
+    return const DiyarExpress();
   }
 }
 
