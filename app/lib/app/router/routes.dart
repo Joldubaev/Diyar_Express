91941d9aa8e43ba4f49ad92b0a1667c50f11e429
admin <admin@admins-MacBook-Pro.local>
2024-03-26 20:14:53 +0600

    fixed conflicts

--- app/lib/app/router/routes.dart
+++ app/lib/app/router/routes.dart
@@ -8,17 +8,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
-
-        AutoRoute(page: MainRoute.page, initial: true, children: [
-          AutoRoute(page: HomeRoute.page),
-          AutoRoute(page: MenuRoute.page),
-          AutoRoute(page: CartRoute.page),
-          AutoRoute(page: ProfileRoute.page)
-        ]),
-
         AutoRoute(
           page: MainRoute.page,
-          initial: true,
           children: [
             AutoRoute(page: HomeRoute.page),
             AutoRoute(page: MenuRoute.page),
@@ -27,19 +18,17 @@ class AppRouter extends $AppRouter {
           ],
           // guards: [AuthGuard()],
         ),
-
         AutoRoute(page: ProfileInfoRoute.page),
         AutoRoute(page: ContactRoute.page),
         AutoRoute(page: OrderHistoryRoute.page),
-        AutoRoute(page: SignInRoute.page),
+        AutoRoute(
+          page: SignInRoute.page,
+          initial: true,
+        ),
         AutoRoute(page: SignUpRoute.page),
-
-
         AutoRoute(page: CreateOrderRoute.page),
         AutoRoute(page: OrderSuccess.page),
         AutoRoute(page: SignUpSucces.page),
-
-
         AutoRoute(page: ContactRoute.page),
         AutoRoute(page: AboutUsRoute.page),
       ];
--- app/lib/constants/api_const/api_const.dart
+++ app/lib/constants/api_const/api_const.dart
@@ -2,11 +2,11 @@ class ApiConst {
   ApiConst._();
 
   // AUTH
-  static const signUp = "auth/sign-up/";
-  static const signIn = "auth/sign-in/";
-  static const refreshToken = "auth/refresh-token/";
-  static const resetPsw = "auth/reset-password/";
-  static const sendCodeToEmail = "auth/send-code-to-email/";
+  static const signUp = "auth/sign-up";
+  static const signIn = "auth/sign-in";
+  static const refreshToken = "auth/refresh-token";
+  static const resetPsw = "auth/reset-password";
+  static const sendCodeToEmail = "auth/send-code-to-email";
 
   // USER
   static const getUser = "user/get-user-info/";
